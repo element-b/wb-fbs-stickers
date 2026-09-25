9e816bae7c437742512e46544f075c0cb6d0a19f
element-b <blinkost@gmail.com>
2026-09-26 01:34:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -379,7 +379,7 @@ def render_login_page(users: dict[str, str]) -> None:
 
     with center_column:
         st.markdown(
-            '<div class="login-title">📦 FBZ</div>',
+            '<div class="login-title"></div>',
             unsafe_allow_html=True,
         )
 
